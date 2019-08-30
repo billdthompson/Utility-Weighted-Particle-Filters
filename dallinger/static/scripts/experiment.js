3397ea2d0c1931515c08b96d96fc280557add81a
Mathew Hardy <matt.hardy@mail.utoronto.ca>
2019-08-30 09:43:13 -0400

    pushing changes

--- dallinger/static/scripts/experiment.js
+++ dallinger/static/scripts/experiment.js
@@ -199,7 +199,7 @@ function make_bar_plot(num_green,num_blue,is_SWI){
     color_code = '#0084ff'
   }
   setTimeout(function(){
-    $('#SWI_info').html("<b id='disclaimer'>DISCLAIMER:</b><br><span id='second-paragraph'>Workers in this group chose emerald more often than workers who weren't paid for emeralds.</span>")
+    $('#SWI_info').html("<b id='disclaimer'>DISCLAIMER:</b><br><span id='second-paragraph'>Workers who were paid for emeralds chose emerald at a higher rate than other workers.</span>")
     $('#SWI_info').css('display','block')
     $('#small_wrapper').css('opacity','0.2')
     setTimeout(function(){
